4599bdf95130822ddcc380454fd282eb36310982
Cinthia.Argote <cinthia.argote1998@gmail.com>
2024-03-08 02:01:41 -0400

    Read files - FileSystem

---
+++ 01-fundamentos/app2.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const data = fs.readFileSync('README.MD', 'utf-8')
+
+const newData = data.replace(/React/ig, 'Angular');
+
+fs.writeFileSync('README-Angular.md', newData);
