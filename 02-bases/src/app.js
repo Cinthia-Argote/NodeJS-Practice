5484407b45b3221aefd9ad6c8c0b529a6c1b0ee3
Cinthia.Argote <cinthia.argote1998@gmail.com>
2024-03-09 01:01:30 -0400

    variables enviroment

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,3 +1,3 @@
 const {emailTemplate} = require('./js-foundation/01-template')
-
-console.log(emailTemplate)
+require('./js-foundation/02-destructuring');
+//console.log(emailTemplate)
---
+++ 02-bases/src/js-foundation/02-destructuring.js
@@ -0,0 +1,13 @@
+console.log(process.env.PORT ?? 3000)
+
+const { SHELL, HOMEBREW_PREFIX } = process.env;
+// console.log(SHELL)
+// console.log(HOMEBREW_PREFIX)
+
+// console.table({ SHELL, HOMEBREW_PREFIX})
+
+const characters = ['Flash', 'Superman', 'Batman']
+
+const [_, __, batman] = characters;
+
+// console.log(batman)
