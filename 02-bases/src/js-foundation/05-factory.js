6427399454bb12ad019d15bea6665bdb7e22ae52
Cinthia.Argote <cinthia.argote1998@gmail.com>
2024-03-10 00:46:22 -0400

    factory 2

--- 02-bases/src/js-foundation/05-factory.js
+++ 02-bases/src/js-foundation/05-factory.js
@@ -1,12 +1,15 @@
 // es una funcion q crea otra funcion
 
+const { v4: uuidv4 } = require('uuid');
+const getAge = require('get-age')
+
 const obj = { name: 'Cinthia', birtthdate: '1998-02-15'}
 const buildPerson = ({ name, birtthdate}) => {
     return {
-        id:  new Date().getTime(),
+        id:  uuidv4(),
         name,
         birtthdate,
-        age: new Date().getFullYear() - new Date(birtthdate).getFullYear()
+        age: getAge(birtthdate)
     }
 }
 
