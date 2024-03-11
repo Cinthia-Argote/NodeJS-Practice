bb8d4c01176d6109f669308ef8924aa12b15c4d6
Cinthia.Argote <cinthia.argote1998@gmail.com>
2024-03-11 14:27:04 -0400

    Moving data

--- 03-typescript/src/app.ts
+++ 03-typescript/src/app.ts
@@ -1,20 +1,22 @@
-const heroes = [
-    {
-        id: 2,
-        name: 'Spiderman',
-        owner: 'Marvel'
-    },
-    {
-        id: 3,
-        name: 'Batman',
-        owner: 'DC'
-    }
+// const heroes = [
+//     {
+//         id: 2,
+//         name: 'Spiderman',
+//         owner: 'Marvel'
+//     },
+//     {
+//         id: 3,
+//         name: 'Batman',
+//         owner: 'DC'
+//     }
 
-]
+import { findHeroById } from "./services/hero.service";
 
-const findHero = (id: number) => {
-    return heroes.find(hero => hero.id === id)
-}
+// ]
 
-const hero = findHero(2)
+// const findHero = (id: number) => {
+//     return heroes.find(hero => hero.id === id)
+// }
+
+const hero = findHeroById(2)
 console.log(hero?.name ?? 'Hero not found!!');
---
+++ 03-typescript/src/data/heroes.ts
@@ -0,0 +1,24 @@
+interface Hero {
+    id: number;
+    name: string;
+    owner: string;
+}
+
+export const heroes = [
+    {
+        id: 1,
+        name: 'IronMan',
+        owner: 'Marvel'
+    },
+    {
+        id: 2,
+        name: 'Spiderman',
+        owner: 'Marvel'
+    },
+    {
+        id: 3,
+        name: 'Batman',
+        owner: 'DC'
+    }
+
+];
